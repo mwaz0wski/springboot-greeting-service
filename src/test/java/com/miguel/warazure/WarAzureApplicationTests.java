c482923fbfeba2e0336eb19e16cf826759ea0e67
mwaz0wski <miguelalbaladejo123@gmail.com>
2022-12-26 12:11:31 +0100

    First commit

---
+++ src/test/java/com/miguel/warazure/WarAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.warazure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
